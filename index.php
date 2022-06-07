327cf02d9f5a3e4ccac19049c608260ffe494213
Dizio <37836244+Dizi0@users.noreply.github.com>
2022-06-07 09:03:09 +0200

    fix small json errors

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ $stages = json_decode($json, true);
 
 //var_dump($stages);
 foreach ($stages as $stage) {
-    echo "<img width='200px' src='".$stage['thumbnail']."'>";
+    echo "<img width='200px' src='".$stage['thumbnail_url']."'>";
     echo '<br>';
     echo $stage['name'] . '<hr>';
 }
