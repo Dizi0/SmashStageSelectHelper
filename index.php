3530598c69f06f416328a115815b381895ff23bf
Dizio <37836244+Dizi0@users.noreply.github.com>
2022-06-07 20:01:05 +0200

    Adding thumbnails, fixed json

--- index.php
+++ index.php
@@ -7,25 +7,6 @@
 </style>
 
 <?php
-function downloadUrlToFile($url, $outFileName)
-{
-    if(is_file($url)) {
-        copy($url, $outFileName);
-    } else {
-        $options = array(
-            CURLOPT_FILE    => fopen($outFileName, 'w'),
-            CURLOPT_TIMEOUT =>  28800, // set this to 8 hours so we dont timeout on big files
-            CURLOPT_URL     => $url
-        );
-
-        $ch = curl_init();
-        curl_setopt_array($ch, $options);
-        curl_exec($ch);
-        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        return $httpcode;
-    }
-}
 
 $json = file_get_contents('./stagelist.json');
 $stages = json_decode($json, true);
@@ -35,7 +16,6 @@ $counter = 0;
 foreach ($stages as $index => $stage) {
     echo $stage['name'];
     echo "<img class='stage' width='50px' src='".$stage['thumbnail_url']."'>";
-    downloadUrlToFile($stage['thumbnail_url'] , $stage['name'] .'.jpg');
 
 //    if ($counter == 11 ){
         echo "<br>";
